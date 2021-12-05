bc914a58f71ded5fbccd0bfd9ce34e581854fc4f
Justin Turner <mrfathed@protonmail.com>
2021-12-05 07:21:24 -0600

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,13 @@
+from math import sqrt
+
 def main():
     #write your code below this line
+    number1 = int(input())
+    number2 = int(input())
+
+    return_value = sqrt(number1 + number2)
+    print(return_value)
+
 
 if __name__ == '__main__':
     main()
